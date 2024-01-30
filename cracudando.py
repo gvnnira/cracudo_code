f4fca75d8316fc1d617facce7f5f3db6731df5ad
gvnnira <g.lira99@outlook.com>
2024-01-30 01:18:26 -0300

    30.01.2024

--- cracudando.py
+++ cracudando.py
@@ -1,5 +1,18 @@
-print("Sucessando");
-print("Acho que deu certo");
-#modificado
-#modificado pelo ubuntu
-number = input("Digite um número: ");
+#Criando variáveis
+produto1 = input("Digite o produto 1: ")
+produto2 = input("Digite o produto 2: ")
+preco1 = 2100,50
+preco2 = 1830,00
+idade = 30
+codigo = 5291
+genero = input("Qual o gênero? ").upper()
+print(f"\nO produto {produto1} custa R$ {preco1}\n"
+      f"O produto {produto2} custa R$ {preco2}\n"
+      f"Codigo: {codigo}\n"
+      f"Dados da pessoa: genero {genero} e idade {idade}\n")
+
+#Criando expressões
+x = int(input("Digite um número: "))
+y = int(input("Digite um número: "))
+mult = x * y
+print(f"O resultado da multiplicação entre {x} e {y} é {mult}")
